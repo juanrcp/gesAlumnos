3e1bfbf1dc1f26bf1436ae1a173d34ba3c0fdf2a
DESKTOP-9402VFR\Juan Ramon Canto <juanr.canto@gmail.com>
2022-11-22 08:49:48 +0100

    Añadimos metodos para insercion, modificacion y borrado en el CRUD de la API 22112022

--- projects/contactos/src/app/contactos.service.ts
+++ projects/contactos/src/app/contactos.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Usuario } from '../../../usuarios/src/app/interfaces/usuario';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,43 @@ export class ContactosService {
     return this.http.get<any>('https://reqres.in/api/users?page=2');
   }
 
-  //PARA INCRUSTAR CON EL DOLAR HAY QUE USAR LA COMILLA INVERSA
+  //PARA INCRUSTAR CON EL DOLAR ($) HAY QUE USAR LA COMILLA INVERSA
   getContacto(id: number) : Observable<any>{
     return this.http.get<any>(`https://reqres.in/api/users/${id}`);
+  }
+
+  //Metodo para comprobar errores
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+  
+      return of(result as T);
+    };
+  }
+  
+  private log(message: string) {
+    console.log(message);
+  }
+
+
+  //Insercion de contacto. El metodo nos pedira donde quiere insertarlo y el que. 
+  //Con el pipe podemos implementar el metodo para ver los errores
+  nuevoContacto(contacto: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://reqres.in/api/users', contacto)
+    .pipe(
+      catchError(this.handleError('nuevoContacto', contacto))
+    );;
+  }
+
+  //Update de contacto. El metodo nos pedira que quiere acturalizar y con que
+  updateContacto(id: number, contacto: Usuario): Observable<Usuario>{
+    return this.http.put<Usuario>(`https://reqres.in/api/users/${id}`, contacto);
+  }
+
+  //Borrar contactos
+  borraContacto(id: number): Observable<number>{
+    return this.http.delete<number>(`https://reqres.in/api/users/${id}`);
   }
   
 }
